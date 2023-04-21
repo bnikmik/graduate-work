c5011fa0a89320af6075f2ff1af39e463980ddd5
Nikita Bolshakov <bnikmik@gmail.com>
2023-04-22 00:47:53 +0300

    тесты

---
+++ src/test/java/ru/skypro/homework/HomeworkApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.skypro.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+class HomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
